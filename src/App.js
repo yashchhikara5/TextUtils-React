dec5d0ac47d05a295b1812eacfa77af683c34094
yashchhikara5 <yashchhikara5@gmail.com>
2022-09-12 20:14:10 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,18 @@ function App() {
     }, 1500);
   }
 
-  const toggleMode = () =>{
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-primary')
+  }
+
+  const toggleMode = (cls) =>{
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = 'rgb(6, 6, 36)';
